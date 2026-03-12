81434ce1809b38d665f1dd22f8f735ddbda98a6b
junjieyang <junjie.yang1@yahoo.com>
2026-03-12 16:30:33 +1100

    Initial Commit

---
+++ comp5703Tests/comp5703Tests.swift
@@ -0,0 +1,16 @@
+//
+//  comp5703Tests.swift
+//  comp5703Tests
+//
+//  Created by 俊杰杨 on 12/3/26.
+//
+
+import Testing
+
+struct comp5703Tests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
---
+++ comp5703UITests/comp5703UITestsLaunchTests.swift
@@ -0,0 +1,33 @@
+//
+//  comp5703UITestsLaunchTests.swift
+//  comp5703UITests
+//
+//  Created by 俊杰杨 on 12/3/26.
+//
+
+import XCTest
+
+final class comp5703UITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    @MainActor
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
